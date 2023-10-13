63ab5628ce8c744ff060cd35c9cb68d6cfc18356
samir <luisvqzlab2022@gmail.com>
2023-10-13 09:16:21 -0500

    se agrego componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componente from './components/C01componente';
+import Variable from './components/P4variable';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Luis Samuel Vasquez Labra <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <C01componente />
+      <Variable xVariable />
     </div>
   );
 }
---
+++ src/components/C01componente.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const C01componente = () => {
+  return (
+    <div>
+      <h1>Componente1</h1>
+      Pagina de prueba
+    </div>
+  )
+}
+
+export default C01componente
